f5fb3ceab5383c71f2eb9f20234f080a9725c642
Costalonga <matteo.costalonga@hpc-head-n1.unitn.it>
2024-11-07 13:29:59 +0100

    Added board sharing between processes

---
+++ MPI/hitori-mpi.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=4:mem=2gb
+#PBS -l walltime=0:05:00
+#PBS -q short_cpuQ
+
+module load mpich-3.2
+cd ./hitori-solver/MPI
+mpirun.actual -n 4 ./hitori-solver-mpi.out
--- MPI/hitori-solver-mpi.c
+++ MPI/hitori-solver-mpi.c
@@ -1,3 +1,4 @@
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,11 +7,14 @@
 #define MAX_BUFFER_SIZE 2048
 #define DEBUG true
 
-int * board; // Original puzzle board
-int rows_count = 0, cols_count = 0; // Number of rows and columns in the puzzle
+enum State {
+    UNASSIGNED = -1,
+    WHITE = 0,
+    BLACK = 1
+};
 
 // Helper function to read a matrix from the input file
-void read_board() {
+void read_board(int **board, int **solution, int *rows_count, int *cols_count) {
     FILE *fp = fopen("../test-cases/inputs/input-5x5.txt", "r");
     
     if (fp == NULL) {
@@ -20,41 +24,47 @@ void read_board() {
 
     char line[MAX_BUFFER_SIZE];
     
+    int rows = 0, cols = 0;
     while (fgets(line, MAX_BUFFER_SIZE, fp)) {
-        rows_count++;
+        rows++;
 
-        if (rows_count == 1) {
+        if (rows == 1) {
             char *token = strtok(line, " ");
             do {
-                cols_count++;
-            } while (token = strtok(NULL, " "));
+                cols++;
+            } while ((token = strtok(NULL, " ")));
         }
     }
 
-    board = malloc((rows_count * cols_count) * sizeof(int));
+    *rows_count = rows;
+    *cols_count = cols;
+
+    *board = (int *) malloc(rows * cols * sizeof(int));
+    *solution = (int *) malloc(rows * cols * sizeof(int));
 
     rewind(fp);
 
-    int rows = 0, cols;
+    int temp_row = 0, temp_col;
     while (fgets(line, MAX_BUFFER_SIZE, fp)) {
         char *token = strtok(line, " ");
 
-        cols = 0;
+        temp_col = 0;
         do {  
-            board[rows * cols_count + cols] = atoi(token);
-            cols++;
-        } while (token = strtok(NULL, " "));
+            (*board)[temp_row * cols + temp_col] = atoi(token);
+            (*solution)[temp_row * cols + temp_col] = UNASSIGNED;
+            temp_col++;
+        } while ((token = strtok(NULL, " ")));
 
-        rows++;
+        temp_row++;
     }
 
     fclose(fp);
 } 
 
-// Helper function to print a matrix
-void print_board() {
+// Helper function to print the board
+void print_board(int *board, int rows_count, int cols_count) {
 
-    if(DEBUG) printf("\nRows: %d, Cols: %d\n", rows_count, cols_count);
+    if (DEBUG) printf("\nRows: %d, Cols: %d\n", rows_count, cols_count);
 
     int i, j;
     for (i = 0; i < rows_count; i++) {
@@ -64,14 +74,128 @@ void print_board() {
         printf("\n");
     }
 
+    printf("\n");
+
     fflush(stdout);
 }
 
-int main() {
+// Helper function to print the solution
+/*void print_solution() {
+
+    if(DEBUG) printf("\nRows: %d, Cols: %d\n", rows_count, cols_count);
+
+    int i, j;
+    for (i = 0; i < rows_count; i++) {
+        for (j = 0; j < cols_count; j++) {
+            if (solution[i * cols_count + j] == WHITE) {
+                printf("O ");
+            } else if (solution[i * cols_count + j] == BLACK) {
+                printf("X ");
+            } else {
+                printf("? ");
+            }
+        }
+        printf("\n");
+    }
+
+    fflush(stdout);
+}
+
+void find_unique_values() {
+
+    int i, j, k;    
+    for(i = 0; i < rows_count; i++) {
+        for(j = 0; j < cols_count; j++) {
+            int value = board[i * cols_count + j];
+            bool unique = true;
+
+            // Check row
+            for(k = 0; k < cols_count; k++) {
+                if(k != j && board[i * cols_count + k] == value) {
+                    unique = false;
+                    break;
+                }
+            }
+
+            if(unique) {
+                // Check column
+                for(k = 0; k < rows_count; k++) {
+                    if(k != i && board[k * cols_count + j] == value) {
+                        unique = false;
+                        break;
+                    }
+                }
+            }
+
+            if (unique)
+                solution[i * cols_count + j] = WHITE;
+        }
+    }
+}
+
+void parallel_find_unique_values() {
+
+
+    MPI_Bcast(&rows_count, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&cols_count, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(board, rows_count * cols_count, MPI_INT, 0, MPI_COMM_WORLD);
+
+    int rows_per_proc = rows_count / size;
+    int start_row = rank * rows_per_proc;
+    int end_row = (rank == size - 1) ? rows_count : start_row + rows_per_proc;
+
+    for(int i = start_row; i < end_row; i++) {
+        for(int j = 0; j < cols_count; j++) {
+            int value = board[i * cols_count + j];
+            bool unique = true;
+
+            for(int k = 0; k < cols_count; k++) {
+                if(k != j && board[i * cols_count + k] == value) {
+                    unique = false;
+                    break;
+                }
+            }
+
+            if(unique) {
+                for(int k = 0; k < rows_count; k++) {
+                    if(k != i && board[k * cols_count + j] == value) {
+                        unique = false;
+                        break;
+                    }
+                }
+            }
+
+            solution[i * cols_count + j] = unique ? WHITE : solution[i * cols_count + j];
+        }
+    }
+
+    MPI_Gather(&solution[start_row * cols_count], (end_row - start_row) * cols_count, MPI_INT,
+               solution, (end_row - start_row) * cols_count, MPI_INT,
+               0, MPI_COMM_WORLD);
+}*/
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int *board; // Original puzzle board
+    int *solution;
+    int rows_count, cols_count;
+
+    if (rank == 0) {
+        read_board(&board, &solution, &rows_count, &cols_count);
+    }
+
+    MPI_Bcast(&rows_count, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&cols_count, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(board, rows_count * cols_count, MPI_INT, 0, MPI_COMM_WORLD);
     
-    read_board();
+    if (DEBUG) printf("Process [%d]", rank);
+    print_board(board, rows_count, cols_count);
 
-    print_board();
-
+    MPI_Finalize();
     return 0;
 }
