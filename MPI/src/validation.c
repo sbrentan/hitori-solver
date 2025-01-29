512bbb7851d9217ead429a9568658e60f1db24d0
Simone Brentan <simobrentan@gmail.com>
2025-01-29 17:26:44 +0100

    Added bfs to hitori validation
    Co-authored-by: wamuumu <matteo68.mc@gmail.com>
    Co-authored-by: Faerye0 <Faerye0@users.noreply.github.com>

--- MPI/src/validation.c
+++ MPI/src/validation.c
@@ -1,7 +1,62 @@
 #include <stdlib.h> 
 #include <string.h>
+#include <stdio.h>
 
 #include "../include/validation.h"
+
+typedef struct {
+    int x;
+    int y;
+} Cell;
+
+typedef struct {
+    Cell *data;
+    int front;
+    int rear;
+    int capacity;
+    int size;
+} ValidationQueue;
+
+ValidationQueue* createQueue(int capacity) {
+    ValidationQueue *queue = (ValidationQueue *)malloc(sizeof(ValidationQueue));
+    queue->data = (Cell *)malloc(capacity * sizeof(Cell));
+    queue->front = 0;
+    queue->rear = -1;
+    queue->capacity = capacity;
+    queue->size = 0;
+    return queue;
+}
+
+bool isValidationQueueEmpty(ValidationQueue *queue) {
+    return queue->size == 0;
+}
+
+void val_enqueue(ValidationQueue *queue, int x, int y) {
+    if (queue->size == queue->capacity) {
+        queue->capacity *= 2;
+        queue->data = (Cell *)realloc(queue->data, queue->capacity * sizeof(Cell));
+    }
+    queue->rear = (queue->rear + 1) % queue->capacity;
+    queue->data[queue->rear].x = x;
+    queue->data[queue->rear].y = y;
+    queue->size++;
+}
+
+Cell val_dequeue(ValidationQueue *queue) {
+    if (isValidationQueueEmpty(queue)) {
+        printf("[ERROR] Queue underflow: Attempting to dequeue from an empty queue.\n");
+        exit(-1);
+    }
+    Cell cell = queue->data[queue->front];
+    queue->front = (queue->front + 1) % queue->capacity;
+    queue->size--;
+    return cell;
+}
+
+void freeQueue(ValidationQueue *queue) {
+    free(queue->data);
+    free(queue);
+}
 
 bool is_cell_state_valid(Board board, BCB* block, int x, int y, CellState cell_state) {
     if (cell_state == BLACK) {
@@ -20,7 +75,36 @@ bool is_cell_state_valid(Board board, BCB* block, int x, int y, CellState cell_s
                 return false;
     }
     return true;
-} 
+}
+
+int bfs_white_cells(Board board, BCB *block, bool *visited, int row, int col) {
+    int directions[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    int count = 0, i;
+
+    ValidationQueue *queue = createQueue(board.rows_count * board.cols_count);
+    val_enqueue(queue, row, col); // assuming the cell is white
+    visited[row * board.cols_count + col] = true;
+
+    while(!isValidationQueueEmpty(queue)) {
+        Cell cell = val_dequeue(queue);
+        count++;
+
+        for (i = 0; i < 4; i++) {
+            int new_row = cell.x + directions[i][0];
+            int new_col = cell.y + directions[i][1];
+
+            if (new_row >= 0 && new_row < board.rows_count && new_col >= 0 && new_col < board.cols_count) {
+                if (!visited[new_row * board.cols_count + new_col] && block->solution[new_row * board.cols_count + new_col] == WHITE) {
+                    val_enqueue(queue, new_row, new_col);
+                    visited[new_row * board.cols_count + new_col] = true;
+                }
+            }
+        }
+    }
+
+    freeQueue(queue);
+    return count;
+}
 
 int dfs_white_cells(Board board, BCB *block, bool* visited, int row, int col) {
     if (row < 0 || row >= board.rows_count || col < 0 || col >= board.cols_count) return 0;
@@ -63,37 +147,14 @@ bool all_white_cells_connected(Board board, BCB* block) {
 
     // Rule 3: When completed, all un-shaded (white) squares create a single continuous area
     return dfs_white_cells(board, block, visited, row, col) == white_cells_count;
+
+    // return bfs_white_cells(board, block, visited, row, col) == white_cells_count;
 }
 
 bool check_hitori_conditions(Board board, BCB* block) {
     
     // Rule 1: No unshaded number appears in a row or column more than once
     // Rule 2: Shaded cells cannot be adjacent, although they can touch at a corner
-
-    int i, j, k;
-    for (i = 0; i < board.rows_count; i++) {
-        for (j = 0; j < board.cols_count; j++) {
-
-            if (block->solution[i * board.cols_count + j] == UNKNOWN) return false;
-
-            if (block->solution[i * board.cols_count + j] == WHITE) {
-                for (k = 0; k < board.rows_count; k++) {
-                    if (k != i && block->solution[k * board.cols_count + j] == WHITE && board.grid[i * board.cols_count + j] == board.grid[k * board.cols_count + j]) return false;
-                }
-
-                for (k = 0; k < board.cols_count; k++) {
-                    if (k != j && block->solution[i * board.cols_count + k] == WHITE && board.grid[i * board.cols_count + j] == board.grid[i * board.cols_count + k]) return false;
-                }
-            }
-
-            if (block->solution[i * board.cols_count + j] == BLACK) {
-                if (i > 0 && block->solution[(i - 1) * board.cols_count + j] == BLACK) return false;
-                if (i < board.rows_count - 1 && block->solution[(i + 1) * board.cols_count + j] == BLACK) return false;
-                if (j > 0 && block->solution[i * board.cols_count + j - 1] == BLACK) return false;
-                if (j < board.cols_count - 1 && block->solution[i * board.cols_count + j + 1] == BLACK) return false;
-            }
-        }
-    }
 
     // Rule 3: When completed, all un-shaded (white) squares create a single continuous area
 
